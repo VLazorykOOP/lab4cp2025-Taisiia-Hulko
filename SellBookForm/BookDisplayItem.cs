ea0acf7aeb1ac9a0c1c0e3591b219fc7eb34ff30
Taisiia-Hulko <162361499+Taisiia-Hulko@users.noreply.github.com>
2025-06-21 15:14:45 +0300

    Add files via upload

---
+++ SellBookForm/BookDisplayItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SellBookForm;
+using BookStoreApp;
+
+namespace BookStoreApp // або твій простір імен
+{
+    public class BookDisplayItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Stock { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ SellBookForm/SellBookForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace BookStoreApp
+
+{
+    partial class SellBookForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbBooks = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudSalePrice = new System.Windows.Forms.NumericUpDown();
+            this.btnSell = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalePrice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbBooks
+            // 
+            this.cmbBooks.FormattingEnabled = true;
+            this.cmbBooks.Location = new System.Drawing.Point(132, 15);
+            this.cmbBooks.Name = "cmbBooks";
+            this.cmbBooks.Size = new System.Drawing.Size(151, 24);
+            this.cmbBooks.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Виберіть книгу";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ціна";
+           
+            // 
+            // nudSalePrice
+            // 
+            this.nudSalePrice.Location = new System.Drawing.Point(132, 51);
+            this.nudSalePrice.Name = "nudSalePrice";
+            this.nudSalePrice.Size = new System.Drawing.Size(151, 22);
+            this.nudSalePrice.TabIndex = 3;
+            // 
+            // btnSell
+            // 
+            this.btnSell.Location = new System.Drawing.Point(171, 79);
+            this.btnSell.Name = "btnSell";
+            this.btnSell.Size = new System.Drawing.Size(112, 23);
+            this.btnSell.TabIndex = 4;
+            this.btnSell.Text = "Підтвердити";
+            this.btnSell.UseVisualStyleBackColor = true;
+            this.btnSell.Click += new System.EventHandler(this.btnSell_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(171, 108);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(112, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Скасувати";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // SellBookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSell);
+            this.Controls.Add(this.nudSalePrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbBooks);
+            this.Name = "SellBookForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSalePrice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBooks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudSalePrice;
+        private System.Windows.Forms.Button btnSell;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
